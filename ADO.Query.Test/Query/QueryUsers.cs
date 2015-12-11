a9459d3c6efa1455f929bf3bed69162a479b1182
omar del valle <omar@odelvalle.com>
2015-12-11 14:59:17 +0100

    Integration test

---
+++ ADO.Query.Test/Query/QueryUsers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ADO.Query.Test.Query
+{
+    using ADO.Query.SqlQuery;
+
+    public class QueryUsers : ISqlQuery
+    {
+        public QueryUsers()
+        {
+            this.Expression = "select id as Id, name as Name from User";
+        }
+
+        public string Expression { get; private set; }
+
+        public IDictionary<string, object> Parameters { get; private set; }
+    }
+}
---
+++ ADO.Query.Test/QueryIntegrationTest.cs
@@ -0,0 +1,34 @@
+﻿namespace ADO.Query.Test
+{
+    using System;
+    using System.IO;
+
+    using ADO.Query.Helper;
+    using ADO.Query.Test.Query;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class QueryIntegrationTest
+    {
+        [ClassInitialize]
+        public static void SetUp(TestContext context)
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        [TestMethod]
+        public void TestIntegrationDataTableQuery()
+        {
+            var queryRunner = QueryRunner.CreateHelper("SqlAdoHelper");
+            var dt = queryRunner.ExecuteDataTable(new QueryUsers());
+
+            Assert.IsNotNull(dt);
+            Assert.IsNotNull(dt.Rows);
+
+            Assert.AreEqual(2, dt.Rows.Count);
+            Assert.AreEqual(1, Convert.ToInt32(dt.Rows[0]["Id"]));
+            Assert.AreEqual("User 1", Convert.ToString(dt.Rows[0]["Name"]));
+        }
+    }
+}

92b5da45622c828eff1a145e0a5c90b27fbb6854
omar del valle <omar@odelvalle.com>
2015-12-14 17:02:40 +0100

    Query Integration test (included localDb)

--- ADO.Query.Test/Query/Dto/SimpleDto.cs
+++ ADO.Query.Test/Query/Dto/SimpleDto.cs
@@ -1,8 +1,12 @@
 ﻿namespace ADO.Query.Test.Query.Dto
 {
+    using System.Collections.Generic;
+
     public class SimpleDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public IEnumerable<PhoneDto> Phones { get; set; } 
     }
 }
